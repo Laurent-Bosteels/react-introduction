a8808026a26609fddbb53e799411ae8677e98cec
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-07-06 10:48:46 +0200

    created header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  const titlename = "My Todo App";
+  return ( 
+
+  <header className="header">
+  <h1>{titlename}</h1>
+  </header>
+  
+  );
+}
+ 
+export default Header;
